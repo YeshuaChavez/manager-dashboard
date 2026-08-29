884147b7e80ec1dde55b62628d0e364f68308f58
YeshuaChavez <yeshua.chavez@unmsm.edu.pe>
2026-08-29 17:35:26 -0500

    ci: deploy to GitHub Pages via Actions (set Vite base path)

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/manager-dashboard/', // requerido para servir la app desde GitHub Pages
   plugins: [react()],
 })
